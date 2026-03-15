162faba083851eca5838521c6869cfc01f5e8166
Kurumii <orionhsu5@hotmail.com>
2026-03-14 21:03:52 -0700

    Update reasoning effort in CreateOpenAiPlan to kMinimal

--- server/src/ai_gateway_planner.cpp
+++ server/src/ai_gateway_planner.cpp
@@ -23,7 +23,7 @@ absl::StatusOr<ExecutionPlan> CreateOpenAiPlan() {
         .step_name = std::string(kMainStepName),
         .system_prompt = *system_prompt,
         .model = std::string(kMainModel),
-        .reasoning_effort = OpenAiReasoningEffort::kNone,
+        .reasoning_effort = OpenAiReasoningEffort::kMinimal,
     });
     return plan;
 }
